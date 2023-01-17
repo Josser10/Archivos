05b83f06943f78fc5c975e27ef041aff42f94422
Josser Borja <joser.borja@udla.edu.ec>
2023-01-17 15:28:57 -0500

    Multiplicacion*3

---
+++ ejemplo.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+
+    FILE *archivo=NULL;
+    FILE *ejemplo=NULL;
+    char buffer[100];
+    double pi;
+    int mult,aux;
+    char buffer1[100];
+
+    archivo=fopen("vector.txt","r+");
+    ejemplo=fopen("multiplicacion.txt","w");
+    
+    if (archivo==NULL){
+        if (ejemplo==NULL){
+            printf("No se puede abrir el archivo");
+            return -1;
+        }   
+    }    
+    while(feof (archivo)){
+        fscanf(archivo, "%d\n",&mult);
+        aux=mult*3;
+        fprintf(ejemplo, "%d\n", aux);
+    }
+     fclose(archivo);
+     fclose(ejemplo);
+
+}
